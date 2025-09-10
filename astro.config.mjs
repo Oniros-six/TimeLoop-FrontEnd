5fe558d7f56d9fad22f04625ad837383035fe33d
Leandro Arrigonni <lean.arrigonni@gmail.com>
2025-09-09 21:13:55 -0300

    chore: :wrench: Relative paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";
 import tunnel from 'astro-tunnel';
+import path from 'path';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
   },
   integrations: [react(), tunnel()],
 });
