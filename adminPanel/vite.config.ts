aa86c29c9bb6e434e88cb1f4bb348f41d7def3bb
Leandro Arrigonni <lean.arrigonni@gmail.com>
2025-09-16 18:33:59 -0300

    feat: :sparkles: Add ShadCn

--- adminPanel/vite.config.ts
+++ adminPanel/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   define: {
     'process.env': process.env
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   plugins: [react(), tailwindcss()],
   base: '/adminPanel/',
